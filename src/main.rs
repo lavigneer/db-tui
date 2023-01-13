8d4a82e0758da942ee03a1225d1fda02f882bf9b
Eric Lavigne <hi_eric@hotmail.com>
2023-01-12 21:01:31 -0500

    Add connection to mongodb and list databases

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
-use cursive::views::{Dialog, TextView};
-use cursive::theme::{Theme, Palette, BorderStyle};
+use std::error::Error;
+use std::env;
+use cursive::theme::{BorderStyle, Palette, Theme};
+use cursive::view::Resizable;
+use cursive::views::{TextView, LinearLayout};
+use mongodb::Client;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> { 
+    let args: Vec<String> = env::args().collect();
+    let mongo_uri = &args[1];
+    let client = Client::with_uri_str(mongo_uri).await?;
+
+    let databases = client.list_databases(None, None).await?;
     // Creates the cursive root - required for every application.
     let mut siv = cursive::default();
     siv.set_theme(Theme {
         shadow: false,
         borders: BorderStyle::Simple,
-        palette: Palette::terminal_default()
+        palette: Palette::terminal_default(),
     });
-    // Creates a dialog with a single "Quit" button
-    siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
-                         .title("Cursive")
-                         .button("Quit", |s| s.quit()));
+
+    let mut main_list_view = LinearLayout::vertical();
+    for database in databases {
+        main_list_view = main_list_view.child( TextView::new(&database.name));
+    }
+
+    siv.add_layer(main_list_view.full_screen());
 
     // Starts the event loop.
     siv.run();
-}
 
+    return Ok(());
+}
