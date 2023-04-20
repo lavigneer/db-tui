5f6830b2e29193c1c88bf80617e9e70ba91155de
Eric Lavigne <hi_eric@hotmail.com>
2023-04-19 22:57:47 -0400

    Set up a simple callback on tree submit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use cursive::theme::{BorderStyle, Color, Palette, PaletteColor, Theme};
 use cursive::view::{Margins, Nameable, Resizable, Scrollable};
 use cursive::views::{LinearLayout, PaddedView, Panel, TextView};
-use cursive::View;
+use cursive::Cursive;
 use cursive_tree_view::{Placement, TreeView};
 use mongodb::Client;
 use std::env;
@@ -11,7 +11,7 @@ use std::error::Error;
 
 extern crate cursive_tree_view;
 
-async fn create_database_list(client: &Client) -> Result<Box<dyn View>, Box<dyn Error>> {
+async fn create_database_tree_view(client: &Client) -> Result<TreeView<String>, Box<dyn Error>> {
     let databases = client.list_databases(None, None).await?;
     let mut tree = TreeView::new();
 
@@ -28,19 +28,14 @@ async fn create_database_list(client: &Client) -> Result<Box<dyn View>, Box<dyn 
             }
         }
     }
+    return Ok(tree);
+}
 
-    let mut main_list_view = LinearLayout::vertical();
-    main_list_view.add_child(PaddedView::new(
-        Margins {
-            left: 1,
-            top: 1,
-            bottom: 1,
-            right: 0,
-        },
-        TextView::new("Databases"),
-    ));
-    main_list_view.add_child(tree.with_name("Databases").scrollable());
-    return Ok(Box::new(main_list_view));
+fn view_database(siv: &mut Cursive, db: String) {
+    siv.call_on_name("database_view", |view: &mut LinearLayout| {
+        view.clear();
+        view.add_child(TextView::new(db));
+    });
 }
 
 #[tokio::main]
@@ -62,10 +57,34 @@ async fn main() -> Result<(), Box<dyn Error>> {
         palette,
     });
 
-    let database_list = create_database_list(&client).await?;
+    let mut database_tree_view = create_database_tree_view(&client).await?;
+    let mut database_tree_layout = LinearLayout::vertical();
+    database_tree_layout.add_child(PaddedView::new(
+        Margins {
+            left: 1,
+            top: 1,
+            bottom: 1,
+            right: 0,
+        },
+        TextView::new("Databases"),
+    ));
+    database_tree_view.set_on_submit(|siv: &mut Cursive, row| {
+        let cb = siv.cb_sink().clone();
+        let value = siv
+            .call_on_name("db_tree", move |tree: &mut TreeView<String>| {
+                return tree.borrow_item(row).unwrap().to_string();
+            })
+            .unwrap();
+
+        cb.send(Box::new(|siv| view_database(siv, value))).unwrap();
+    });
+    database_tree_layout.add_child(database_tree_view.with_name("db_tree").scrollable());
+
     let mut main_view = LinearLayout::horizontal();
-    main_view.add_child(database_list);
-    siv.add_layer(Panel::new(main_view.full_screen()));
+    main_view.add_child(database_tree_layout);
+    let database_view = LinearLayout::vertical();
+    main_view.add_child(database_view.with_name("database_view"));
+    siv.add_layer(Panel::new(main_view.full_screen().with_name("main")));
 
     // Starts the event loop.
     siv.run();
